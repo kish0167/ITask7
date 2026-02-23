37705d09fb1ce30cb4efb7d110f492a0b9b75e85
kish <simerkish@gmail.com>
2026-02-24 00:05:06 +0400

    users blocking/unblocking

--- Controllers/AdminPanelController.cs
+++ Controllers/AdminPanelController.cs
@@ -1,4 +1,5 @@
 ﻿using ITask7.Data;
+using ITask7.Services;
 using ITask7.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,12 +12,58 @@ public class AdminPanelController(ApplicationDbContext dbContext)
 {
     private readonly ApplicationDbContext _dbContext = dbContext;
 
+    //[Authorize(Roles = "Admin")]
     public async Task<IActionResult> Index()
     {
         var userViewModels = await GetViewModelsFromDb();
         return View(userViewModels);
     }
+    
+    [HttpPost]
+    //[Authorize(Roles = "Admin")]
+    public async Task<IActionResult> BulkDelete([FromBody] List<string> userIds)
+    {
+        int deleted = await _dbContext.Users
+            .Where(u => userIds.Contains(u.Id))
+            .ExecuteDeleteAsync();
+        return Ok(new { deleted });
+    }
 
+    [HttpPost]
+    //[Authorize(Roles = "Admin")]
+    public async Task<IActionResult> BulkBlock([FromBody] List<string> userIds)
+    {
+        string? blockedRoleId = await _dbContext.Roles
+            .Where(r => r.Name == UserRoles.Blocked)
+            .Select(r => r.Id)
+            .FirstOrDefaultAsync();
+        
+        var newUserRoles = userIds.Select(userId => new IdentityUserRole<string>
+        {
+            UserId = userId,
+            RoleId = blockedRoleId ?? throw new Exception("role id not found")
+        });
+
+        await _dbContext.UserRoles.AddRangeAsync(newUserRoles);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(new { blocked = userIds.Count });
+    }
+    
+    [HttpPost]
+    //[Authorize(Roles = "Admin")]
+    public async Task<IActionResult> BulkUnblock([FromBody] List<string> userIds)
+    {
+        string? blockedRoleId = await _dbContext.Roles
+            .Where(r => r.Name == UserRoles.Blocked)
+            .Select(r => r.Id)
+            .FirstOrDefaultAsync();
+        int deletedCount = await _dbContext.UserRoles
+            .Where(ur => userIds.Contains(ur.UserId) && ur.RoleId == blockedRoleId)
+            .ExecuteDeleteAsync();
+        return Ok(new { deletedCount });
+    }
+    
     private async Task<List<UserViewModel>> GetViewModelsFromDb()
     {
         var userViewModels = await _dbContext.Users
