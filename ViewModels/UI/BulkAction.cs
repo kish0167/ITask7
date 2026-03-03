37493e164d85a0594add93791d11226a5d4fae30
kish <simerkish@gmail.com>
2026-03-03 19:48:19 +0400

    bulkaction toolbar generalization

---
+++ ViewModels/UI/BulkAction.cs
@@ -0,0 +1,10 @@
+﻿namespace ITask7.ViewModels.UI;
+
+public class BulkAction
+{
+    public string Name { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Icon { get; set; } = null!;
+    public string ButtonClass { get; set; } = "btn-outline-secondary";
+    public string Endpoint { get; set; } = null!;
+}
---
+++ ViewModels/UI/BulkActionToolbarViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ITask7.ViewModels.UI;
+
+public class BulkActionToolbarViewModel
+{
+    public List<BulkAction> Actions { get; set; } = new();
+}
