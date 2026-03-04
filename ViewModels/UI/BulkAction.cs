4864d6225bdd523729435d1858e1dd2b84abf539
kish <simerkish@gmail.com>
2026-03-04 13:22:50 +0400

    toolbar fix

--- ViewModels/UI/BulkAction.cs
+++ ViewModels/UI/BulkAction.cs
@@ -4,6 +4,7 @@ public class BulkAction
 {
     public string Name { get; set; } = null!;
     public string Title { get; set; } = null!;
+    public bool IsSingle { get; set; } = false;
     public string Icon { get; set; } = null!;
     public string ButtonClass { get; set; } = "btn-outline-secondary";
     public string Endpoint { get; set; } = null!;
